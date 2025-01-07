f2bd4fcb7fb038d140d2147d2543b762fd582762
Stefan Seifert <stefanseifert@users.noreply.github.com>
2025-01-07 21:39:50 +0100

    Prosperity Phase: Show drafted cards

--- src/views/PhaseBProsperity.vue
+++ src/views/PhaseBProsperity.vue
@@ -5,6 +5,10 @@
 
   <p class="mt-4" v-html="t('phaseBProsperity.skip')"></p>
 
+  <div v-if="playerTechs.length > 0" class="mt-3">
+    <TechCardsPlayerDraft :navigationState="navigationState" :playerTechs="playerTechs"/>
+  </div>
+
   <button class="btn btn-primary btn-lg mt-4" @click="next()">
     {{t('action.next')}}
   </button>
@@ -19,16 +23,19 @@ import { defineComponent } from 'vue'
 import { useI18n } from 'vue-i18n'
 import FooterButtons from '@/components/structure/FooterButtons.vue'
 import { useRoute } from 'vue-router'
-import { useStateStore } from '@/store/state'
+import { Round, useStateStore } from '@/store/state'
 import SideBar from '@/components/round/SideBar.vue'
 import NavigationState from '@/util/NavigationState'
 import DebugInfo from '@/components/round/DebugInfo.vue'
+import TechCardsPlayerDraft from '@/components/round/TechCardsPlayerDraft.vue'
+import Tech from '@/services/enum/Tech'
 
 export default defineComponent({
   name: 'PhaseBProsperity',
   components: {
     FooterButtons,
     SideBar,
+    TechCardsPlayerDraft,
     DebugInfo
   },
   setup() {
@@ -44,6 +51,12 @@ export default defineComponent({
   computed: {
     backButtonRouteTo() : string {
       return `/round/${this.round}/drafting`
+    },
+    roundData() : Round|undefined {
+      return this.state.rounds.find(item => item.round == this.navigationState.round)
+    },
+    playerTechs() : Tech[] {
+      return this.roundData?.playerTechs ?? []
     }
   },
   methods: {
