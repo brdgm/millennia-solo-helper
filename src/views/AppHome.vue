83bc695de7c090597cd4db93aab663f4e3670326
Stefan Seifert <stefanseifert@users.noreply.github.com>
2025-01-28 17:01:51 +0100

    Rename to Millennia: Tracks of Time

--- src/views/AppHome.vue
+++ src/views/AppHome.vue
@@ -21,7 +21,7 @@
 
   <p class="disclaimer">
     This is not an official Karma Games product and has no affiliation with <a href="https://karma-games.com/" target="_blank" rel="noopener">Karma Games</a>.
-    All art in this app is from <a href="https://karma-games.com/millennia/" target="_blank" rel="noopener">Millennia</a>.
+    All art in this app is from <a href="https://karma-games.com/millennia/" target="_blank" rel="noopener">Millennia: Tracks of Time</a>.
     See <a href="#" data-bs-toggle="modal" data-bs-target="#creditsModal">{{t('footer.credits')}}</a>.
     <a href="https://ko-fi.com/brdgm_me" target="_blank" rel="noopener"><img class="kofi-button" src="@/assets/kofi_button_dark.png" alt="Support me on Ko-fi"/></a>
   </p>
